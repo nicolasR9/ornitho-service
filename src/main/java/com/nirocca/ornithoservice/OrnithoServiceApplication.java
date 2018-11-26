15ff64e02168434f52847793b79b2a3b1083c119
Nicolas Rocca <nirocca@team.mobile.de>
2018-11-26 13:20:59 +0100

    first commit

---
+++ src/main/java/com/nirocca/ornithoservice/OrnithoServiceApplication.java
@@ -0,0 +1,12 @@
+package com.nirocca.ornithoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrnithoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrnithoServiceApplication.class, args);
+	}
+}
